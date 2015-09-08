ac32f5b79cf8685e51532d3ea76ffe3f48b6d72c
芮峰云 <rfyiamcool@163.com>
2015-09-09 01:23:59 +0800

    clean format

--- dburi/dburi.py
+++ dburi/dburi.py
@@ -63,11 +63,3 @@ def parse_db_str(conn_str):
         raise ArgumentError(
             "Could not parse rfc1738 URL from string '%s', the format should match 'dialect+driver://username:password@host:port/database'" % name)
 
-if __name__ == "__main__":
-    print parse_db_str('mysql://buzz:123@localhost/buzz_master?option=open&charset=utf8&table=test')
-    print parse_db_str('mysql://buzz:123@localhost/buzz_master')
-    print parse_db_str("mongo://127.0.0.1:27017/buzz_master")
-    print parse_db_str('redis://127.0.0.1:6379')
-    print parse_db_str('elasticsearch://127.0.0.1:9200')
-    print parse_db_str('hbase://127.0.0.1:9090/buzz')
-    print parse_db_str('memcached://127.0.0.1:11211')
